66c7fc687c68427d967ffa2e2da274b2bdfc30c9
Sidnym Ladrut <sidnym@ladrut.xyz>
2025-11-07 21:41:00 +0000

    add basic 'Fund.withdraw' implementation (+ tests)

--- packages/chain/contracts/Fund.sol
+++ packages/chain/contracts/Fund.sol
@@ -3,11 +3,19 @@ pragma solidity ^0.8.28;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
-contract Fund is Ownable {
+contract Fund is Ownable, EIP712 {
+  ////////////////////////
+  // Constant Variables //
+  ////////////////////////
+
+  bytes32 private constant _WITHDRAW_TYPEHASH = keccak256("Withdraw(uint256 amount,uint256 nonce)");
+  uint256 private constant _CUT_MAXIMUM = 1e4;
+
   /////////////////////
   // State Variables //
   /////////////////////
@@ -23,11 +31,13 @@ contract Fund is Ownable {
   ERC20Permit public payoutToken;
   // @notice The IPFS CID of the JSON file with the terms of the work
   bytes32 public termsCID;
+  // @notice The nonce for the next withdrawal
+  uint8 private _nonce;
 
-  // TODO: Think about how to implement refunds in this model (probably need
-  // another mapping)
   // @notice A local record of the funds deposited into this contract (by ERC20)
-  mapping(ERC20Permit => uint256) public funds;
+  mapping(ERC20Permit => uint256) public treasury;
+  mapping(ERC20Permit => bool) private _treasuryTokenMap;
+  ERC20Permit[] treasuryTokens;
   // @notice The oracle's signature on the work terms, which seals the fund
   bytes public termsSignature;
 
@@ -39,10 +49,18 @@ contract Fund is Ownable {
   event Deposit(ERC20Permit indexed token, address indexed from, uint256 amount);
   // @notice Notification for when tokens are withdrawn from the fund
   event Withdrawal(uint256 amount);
+  // @notice Notification for when a refund is issued
+  event Refund(address indexed refunder);
 
   ///////////////
   // Modifiers //
   ///////////////
+
+  // TODO: natspec
+  modifier onlyManager() {
+    require(msg.sender == owner() || msg.sender == oracle, "Not a fund manager (i.e. worker or oracle)");
+    _;
+  }
 
   // TODO: natspec
   modifier beforeLocked() {
@@ -62,13 +80,13 @@ contract Fund is Ownable {
 
   // TODO: natspec
   constructor(address oracle_, uint256 cut, ERC20Permit token, bytes32 terms)
-      Ownable(msg.sender) {
+      Ownable(msg.sender) EIP712("Fund", "1") {
     updateTerms(oracle_, cut, token, terms);
   }
 
   // TODO: natspec
   function worker() external view returns (address) {
-      return owner();
+    return owner();
   }
 
   // TODO: natspec
@@ -81,30 +99,86 @@ contract Fund is Ownable {
   }
 
   // TODO: natspec
-  function lockTerms(bytes memory signature) public beforeLocked {
+  function lockTerms(bytes memory oracleSignature) public onlyOwner beforeLocked {
     (address signer, ECDSA.RecoverError error, ) = ECDSA.tryRecover(
       MessageHashUtils.toEthSignedMessageHash(termsCID),
-      signature
+      oracleSignature
     );
-    require(error == ECDSA.RecoverError.NoError, "Bad signature");
-    require(signer == oracle, "Mismatched signer");
-    termsSignature = signature;
+    require(error == ECDSA.RecoverError.NoError, "Malformed signature provided");
+    require(signer == oracle, "Invalid signer provided (must be the contract oracle)");
+    termsSignature = oracleSignature;
   }
 
   // TODO: natspec
-  function deposit(ERC20Permit token, address funder, uint256 amount, bytes memory signature)
+  function deposit(ERC20Permit token, address funder, uint256 amount, bytes memory funderSignature)
       public afterLocked {
+    // TODO: Remove
+    require(token == payoutToken, "Only deposits in the contract's payout token are currently accepted");
+
+    // TODO: Refactor into standalone function (?)
     bytes32 r;
     bytes32 s;
     uint8 v;
     assembly ("memory-safe") {
-      r := mload(add(signature, 0x20))
-      s := mload(add(signature, 0x40))
-      v := byte(0, mload(add(signature, 0x60)))
+      r := mload(add(funderSignature, 0x20))
+      s := mload(add(funderSignature, 0x40))
+      v := byte(0, mload(add(funderSignature, 0x60)))
     }
 
     token.permit(funder, address(this), amount, block.timestamp, v, r, s);
     token.transferFrom(funder, address(this), amount);
+
+    if (!_treasuryTokenMap[token]) {
+      treasuryTokens.push(token);
+      _treasuryTokenMap[token] = true;
+    }
+    treasury[token] += amount;
+
+    emit Deposit(token, funder, amount);
+  }
+
+  // TODO: natspec
+  function withdraw(uint256 amount, bytes memory oracleSignature)
+      public onlyOwner afterLocked {
+    require(amount > 0, "Must withdraw a non-zero sum");
+    require(amount <= funds(), "Overdraft on the existing funds");
+
+    (address signer, ECDSA.RecoverError error, ) = ECDSA.tryRecover(hashWithdraw(amount), oracleSignature);
+    require(error == ECDSA.RecoverError.NoError, "Malformed signature provided");
+    require(signer == oracle, "Invalid signer provided (must be the contract oracle)");
+
+    uint256 oracleAmount = (amount * oracleCut) / _CUT_MAXIMUM;
+    payoutToken.transfer(owner(), amount - oracleAmount);
+    payoutToken.transfer(oracle, oracleAmount);
+    treasury[payoutToken] -= amount;
+    _nonce++;
+
+    emit Withdrawal(amount);
+  }
+
+  // TODO: natspec
+  function refund() public onlyManager afterLocked {
+    // TODO: Close out the fund (set a flag in an existing variable)
+    emit Refund(msg.sender);
+  }
+
+  // TODO: natspec
+  function funds() public view returns (uint256 amount) {
+    amount = 0;
+    for (uint256 i = 0; i < treasuryTokens.length; i++) {
+      amount += treasury[treasuryTokens[i]];
+    }
+    return amount;
+  }
+
+  //////////////////////
+  // Helper Functions //
+  //////////////////////
+
+  function hashWithdraw(uint256 amount) public view returns (bytes32) {
+    bytes32 structHash = keccak256(abi.encode(_WITHDRAW_TYPEHASH, amount, _nonce));
+    bytes32 hash = _hashTypedDataV4(structHash);
+    return hash;
   }
 
   // TODO: natspec
--- packages/chain/test/FundTest.t.sol
+++ packages/chain/test/FundTest.t.sol
@@ -41,10 +41,11 @@ contract FundTest is Test {
   uint256 constant ORACLE_PK = 2;
   uint256 constant FUNDER_PK = 3;
 
-  uint256 constant FUND_CUT = 1e3;
+  uint256 constant FUND_CUT = 1e3; // 10%
   bytes32 constant FUND_TERMS = bytes32(uint256(100));
   bytes32 constant BAD_TERMS = bytes32(uint256(101));
   uint256 constant DEPO_AMOUNT = 1e1 * 10 ** TOKEN_DECIMALS;
+  uint256 constant DEPO_CUT = 1e0 * 10 ** TOKEN_DECIMALS;
 
   Fund fund;
   FundToken token;
@@ -67,13 +68,19 @@ contract FundTest is Test {
 
     vm.prank(worker);
     fund = new Fund(oracle, FUND_CUT, token, FUND_TERMS);
-
     vm.prank(worker);
   }
 
   modifier locked() {
-    bytes memory signature = signAs191(FUND_TERMS, ORACLE_PK);
+    bytes memory signature = _signAs191(FUND_TERMS, ORACLE_PK);
     fund.lockTerms(signature);
+    _;
+  }
+
+  modifier funded() {
+    bytes32 permitHash = token.hashPermit(funder, address(fund), DEPO_AMOUNT, block.timestamp);
+    bytes memory funderDepositSignature = _signAsRaw(permitHash, FUNDER_PK);
+    fund.deposit(token, funder, DEPO_AMOUNT, funderDepositSignature);
     _;
   }
 
@@ -88,25 +95,25 @@ contract FundTest is Test {
   }
 
   function test_lockTerms_success() public {
-    bytes memory oracleTermsSignature = signAs191(FUND_TERMS, ORACLE_PK);
+    bytes memory oracleTermsSignature = _signAs191(FUND_TERMS, ORACLE_PK);
     fund.lockTerms(oracleTermsSignature);
     assertEq(fund.termsSignature(), oracleTermsSignature);
   }
 
-  function test_lockTerms_badAuthor() public {
-    bytes memory workerTermsSignature = signAs191(FUND_TERMS, WORKER_PK);
+  function test_lockTerms_badSigner() public {
+    bytes memory workerTermsSignature = _signAs191(FUND_TERMS, WORKER_PK);
     vm.expectRevert();
     fund.lockTerms(workerTermsSignature);
   }
 
   function test_lockTerms_badMessage() public {
-    bytes memory oracleRandomSignature = signAs191(BAD_TERMS, ORACLE_PK);
+    bytes memory oracleRandomSignature = _signAs191(BAD_TERMS, ORACLE_PK);
     vm.expectRevert();
     fund.lockTerms(oracleRandomSignature);
   }
 
   function test_lockTerms_postLock() public locked {
-    bytes memory oracleTermsSignature = signAs191(FUND_TERMS, ORACLE_PK);
+    bytes memory oracleTermsSignature = _signAs191(FUND_TERMS, ORACLE_PK);
     vm.expectRevert();
     fund.lockTerms(oracleTermsSignature);
     vm.expectRevert();
@@ -115,27 +122,76 @@ contract FundTest is Test {
 
   function test_deposit_success() public locked {
     bytes32 permitHash = token.hashPermit(funder, address(fund), DEPO_AMOUNT, block.timestamp);
-    bytes memory funderDepositSignature = signAsRaw(permitHash, FUNDER_PK);
+    bytes memory funderDepositSignature = _signAsRaw(permitHash, FUNDER_PK);
+
+    vm.expectEmit();
+    emit Fund.Deposit(token, funder, DEPO_AMOUNT);
     fund.deposit(token, funder, DEPO_AMOUNT, funderDepositSignature);
-    assertEq(TOKEN_SUPPLY - DEPO_AMOUNT, token.balanceOf(funder));
+
+    assertEq(token.balanceOf(funder), TOKEN_SUPPLY - DEPO_AMOUNT);
+    assertEq(fund.funds(), DEPO_AMOUNT);
   }
 
   function test_deposit_badPermit() public locked {
-    bytes memory funderDepositSignature = signAsRaw(BAD_TERMS, FUNDER_PK);
+    bytes memory funderDepositSignature = _signAsRaw(BAD_TERMS, FUNDER_PK);
     vm.expectRevert();
     fund.deposit(token, funder, DEPO_AMOUNT, funderDepositSignature);
+  }
+
+  function test_withdraw_success() public locked funded {
+    bytes memory oracleWithdrawalSignature = _signAsRaw(fund.hashWithdraw(DEPO_AMOUNT), ORACLE_PK);
+
+    vm.prank(worker);
+    vm.expectEmit();
+    emit Fund.Withdrawal(DEPO_AMOUNT);
+    fund.withdraw(DEPO_AMOUNT, oracleWithdrawalSignature);
+
+    assertEq(token.balanceOf(worker), DEPO_AMOUNT - DEPO_CUT);
+    assertEq(token.balanceOf(oracle), DEPO_CUT);
+    assertEq(token.balanceOf(address(fund)), 0);
+  }
+
+  function test_withdraw_badSigner() public locked funded {
+    bytes memory workerWithdrawalSignature = _signAsRaw(fund.hashWithdraw(DEPO_AMOUNT), WORKER_PK);
+    vm.prank(worker);
+    vm.expectRevert();
+    fund.withdraw(DEPO_AMOUNT, workerWithdrawalSignature);
+  }
+
+  function test_withdraw_badMessage() public locked funded {
+    bytes memory oracleRandomSignature = _signAs191(BAD_TERMS, ORACLE_PK);
+    vm.prank(worker);
+    vm.expectRevert();
+    fund.withdraw(DEPO_AMOUNT, oracleRandomSignature);
+  }
+
+  function test_withdraw_badAmount() public locked funded {
+    bytes memory oracleRealWithdrawalSignature = _signAsRaw(fund.hashWithdraw(DEPO_AMOUNT), ORACLE_PK);
+    bytes memory oracleZeroWithdrawalSignature = _signAsRaw(fund.hashWithdraw(0), ORACLE_PK);
+
+    vm.prank(worker);
+    vm.expectRevert();
+    fund.withdraw(DEPO_AMOUNT + 1, oracleRealWithdrawalSignature);
+
+    vm.prank(worker);
+    vm.expectRevert();
+    fund.withdraw(DEPO_AMOUNT - 1, oracleRealWithdrawalSignature);
+
+    vm.prank(worker);
+    vm.expectRevert();
+    fund.withdraw(0, oracleZeroWithdrawalSignature);
   }
 
   //////////////////////
   // Helper Functions //
   //////////////////////
 
-  function signAs191(bytes32 data, uint256 pk) private pure returns (bytes memory signature) {
+  function _signAs191(bytes32 data, uint256 pk) internal pure returns (bytes memory signature) {
     bytes32 hash = MessageHashUtils.toEthSignedMessageHash(data);
-    return signAsRaw(hash, pk);
+    return _signAsRaw(hash, pk);
   }
 
-  function signAsRaw(bytes32 data, uint256 pk) private pure returns (bytes memory signature) {
+  function _signAsRaw(bytes32 data, uint256 pk) internal pure returns (bytes memory signature) {
     (uint8 v, bytes32 r, bytes32 s) = vm.sign(pk, data);
     return abi.encodePacked(r, s, v);
   }
