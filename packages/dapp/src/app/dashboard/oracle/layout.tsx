3ad9501655ce7dcf285163796376bd8150b3250e
Sidnym Ladrut <sidnym@ladrut.xyz>
2025-11-21 17:51:03 +0000

    ux: add header titles for all pages

---
+++ packages/dapp/src/app/dashboard/oracle/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata, ResolvedMetadata, ResolvingMetadata } from 'next';
+
+// https://nextjs.org/docs/app/api-reference/functions/generate-metadata#generatemetadata-function
+export async function generateMetadata(
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const meta: ResolvedMetadata = (await parent);
+  return {
+    title: 're-fund | oracle dashboard',
+    description: meta.description,
+  };
+}
+
+export default function FundLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
